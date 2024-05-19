297de3c26f3daa1a4b67c2704d2f7f357a97d0b4
Ilaha1996 <ilahaah@code.edu.az>
2024-05-19 22:41:45 +0400

    added tasks

---
+++ RemoveSpaces/StringWithoutSpace/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics.Tracing;
+
+namespace StringWithoutSpace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "Code Academy";
+            Console.WriteLine(RemoveSpace(word));
+        }
+
+        static string RemoveSpace(string input)
+        {
+            
+            string result = "";
+
+            foreach (char c in input) 
+            {
+                if (c != ' ')
+                { 
+                  result += c;
+
+                }
+                           
+            
+            }
+
+            return result;
+
+            
+        }
+    }
+}
---
+++ Sumofoddnums/Sumofoddnums/Program.cs
@@ -0,0 +1,67 @@
+﻿using System.Globalization;
+
+namespace Sumofoddnums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(SumOfOddNumbers(1,2,3, 4,5,6,7));
+            
+
+          
+            Sumofoddnumbers1(1, 2, 3, 4, 5, 6, 7,8,9,10,11,12,13);
+        }
+
+        static int SumOfOddNumbers(params int[] numbers)
+        {
+
+           int sum = 0;
+            
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] %2==0)
+                {
+                    sum += numbers[i];
+
+                }
+               
+            }
+
+            return sum;
+
+
+        }
+
+        static void Sumofoddnumbers1(params int[] nums)
+        {
+            
+
+            int sum1= 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {   
+                if (nums[i] % 2 == 0)
+                {
+                    sum1 += nums[i];
+                    
+                }                               
+                
+            }
+            Console.WriteLine(sum1);
+
+
+        }
+
+    }
+
+}
+        
+
+        
+          
+
+    
+
